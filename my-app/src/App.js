26209d8b069c44293a1427268aff0e6159701d2e
Ayush Vidhale <ayushvidhale099@gmail.com>
2023-04-24 12:59:17 +0530

    boiler code

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNavbar from '../components/Navbar';
+import React from 'react';
+
+function App() {
+  return (
+   <>
+   <MyNavbar/>
+   </>
+   
+  );
+}
+
+export default App;
